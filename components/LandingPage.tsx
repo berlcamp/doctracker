39ec76df8877dee05e506c8e33da8a515805738f
Berl Treasure Campomanes <berlcamp@gmail.com>
2024-01-16 13:25:24 +0800

    ..

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,11 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 'use client'
 import { StatusFlow, TopBarDark } from '@/components'
 import { useState } from 'react'
 import { createBrowserClient } from '@supabase/ssr'
+import type { DocumentTypes } from '@/types'
+import { statusList } from '@/constants/TrackerConstants'
 
 export default function LandingPage () {
   const [routingNo, setRoutingNo] = useState('')
-  const [documentId, setDocumentId] = useState('')
+  const [documentData, setDocumentData] = useState<DocumentTypes | null>(null)
   const [notFound, setNotFound] = useState(false)
 
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -16,24 +19,32 @@ export default function LandingPage () {
 
     if (routingNo.trim() === '') {
       setRoutingNo('')
-      setDocumentId('')
+      setDocumentData(null)
       return
     }
 
     const { data } = await supabase
       .from('dum_document_trackers')
-      .select()
+      .select('*, dum_document_tracker_stickies(*), dum_document_followers(*),dum_users:user_id(*),received_by_user:received_by(id,name,avatar_url),current_department:current_department_id(id,name),dum_departments:origin_department_id(name),dum_remarks(*)')
       .eq('routing_slip_no', routingNo)
       .limit(1)
       .single()
 
     if (data) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      setDocumentId(data.id.toString())
+      setDocumentData(data)
       setNotFound(false)
     } else {
-      setDocumentId('')
       setNotFound(true)
+      setDocumentData(null)
+    }
+  }
+
+  const getStatusColor = (status: string): string => {
+    const statusArr = statusList.filter(item => item.status === status)
+    if (statusArr.length > 0) {
+      return statusArr[0].color
+    } else {
+      return '#000000'
     }
   }
 
@@ -61,9 +72,21 @@ export default function LandingPage () {
           notFound &&
             <div className='my-10 text-center text-gray-700'>No document matched to this Routing No.</div>
         }
-        <div className='w-full px-10 my-10'>
+        <div className='w-full px-10 my-10 pb-20'>
           {
-            documentId !== '' && <StatusFlow documentId={documentId} updateStatusFlow={true}/>
+            (documentData) &&
+              <>
+                <div className='pl-4 mb-2'>
+                  <span className='text-gray-700 text-sm'>Document Type: </span>
+                  <span className='font-bold text-sm'>{documentData.type}</span>
+                </div>
+                <div className='pl-4 mb-2'>
+                  <span className='text-gray-700 text-sm'>Current Status: </span>
+                  <span className='font-bold text-sm'>{documentData.current_status} {documentData.current_status === 'Forwarded' ? 'to' : 'at'}  {documentData.current_department.name}</span>
+                </div>
+                <div className='pl-4 mt-8 mb-6 font-bold text-gray-700'>Tracker</div>
+                <StatusFlow documentId={documentData.id} updateStatusFlow={true}/>
+              </>
           }
         </div>
         <div className='mt-auto bg-gray-800 p-4 text-white fixed bottom-0 w-full'>
