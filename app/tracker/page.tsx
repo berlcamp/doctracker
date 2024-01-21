d1f3108e10ec84f0f9cfdf30bf7ef29ee2166264
Berl Treasure Campomanes <berlcamp@gmail.com>
2024-01-21 08:28:21 +0800

    ..

--- app/tracker/page.tsx
+++ app/tracker/page.tsx
@@ -272,7 +272,7 @@ const Page: React.FC = () => {
                                   </div>
                                 </Menu.Item>
                                 {
-                                  (session.user.id.toString() === item.user_id.toString() && item.status === 'Tracker Created') &&
+                                  (session.user.id.toString() === item.user_id.toString() && item.current_status === 'Tracker Created') &&
                                     <Menu.Item>
                                       <div
                                           onClick={ () => handleDelete(item.id) }
